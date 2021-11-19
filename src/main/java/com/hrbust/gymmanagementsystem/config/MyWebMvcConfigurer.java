39f79612f3c29d645adff4e7df1c6becd7b12609
JinWeimin <jinweimin@null.com>
2021-11-19 20:45:23 +0800

    feat:add resources handler

--- src/main/java/com/hrbust/gymmanagementsystem/config/MyWebMvcConfigurer.java
+++ src/main/java/com/hrbust/gymmanagementsystem/config/MyWebMvcConfigurer.java
@@ -3,6 +3,7 @@ package com.hrbust.gymmanagementsystem.config;
 import com.hrbust.gymmanagementsystem.filter.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/").setViewName("login");
+    registry.addViewController("/").setViewName("WEB-INF/views/login.jsp");
   }
 
   @Override
@@ -20,5 +21,10 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
         .addInterceptor(new LoginInterceptor())
         .addPathPatterns("/**")
         .excludePathPatterns("/login", "/login/handler");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/views/**").addResourceLocations("classpath:/views/");
   }
 }
