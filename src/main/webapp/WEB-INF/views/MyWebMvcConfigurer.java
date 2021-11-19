67fd61abcec264cc47da51c1d94e1a8a02ce6083
JinWeimin <jinweimin@null.com>
2021-11-19 20:50:07 +0800

    fix:fix resources handler

--- src/main/webapp/WEB-INF/views/MyWebMvcConfigurer.java
+++ src/main/webapp/WEB-INF/views/MyWebMvcConfigurer.java
@@ -25,6 +25,6 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/views/**").addResourceLocations("classpath:/views/");
+    registry.addResourceHandler("WEB-INF/views/**").addResourceLocations("WEB-INF/views/");
   }
 }
